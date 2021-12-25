07a55dd5c731477df7a1b80169ce1c7fd845cc0d
Seyfullayev <tu201906250@aztu.local>
2021-12-25 12:04:21 +0400

    project added

---
+++ EduhomeTemplate/Areas/Manage/Controllers/NoticeController.cs
@@ -0,0 +1,40 @@
+﻿using EduhomeTemplate.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduhomeTemplate.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class NoticeController : Controller
+    {
+        private readonly DataContext _context;
+
+        public NoticeController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            return View(_context.Notices.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Notice notice)
+        {
+            _context.Notices.Add(notice);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("index", "notice");
+        }
+    }
+}
